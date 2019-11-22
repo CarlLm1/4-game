5a5eab872491b87245cebfc22165eb6e1c3b8858
Carl Medina <carl.l.medina@comcast.net>
2019-11-21 18:14:40 -0800

    creating logic with jquery

--- assets/javascript/rpgStarWars.js
+++ assets/javascript/rpgStarWars.js
@@ -31,4 +31,37 @@ $(document).ready(function() {
   };
   // Checking if object and all properties are loading
   console.log(characters);
+
+  //The following function will render a character card to the page.
+  var renderOne = function(character, renderArea) {
+    var charDiv = $(
+      "<div class = 'character' data-name = '" + character.name + "'>"
+    );
+    var charName = $("<div class = 'character-name'>").text(character.name);
+    var charImage = $("<img alt = 'image' class = 'character-image'>").attr(
+      "src",
+      character.imageUrl
+    );
+    var charHealth = $("<div class ='character-health'>").text(
+      character.health
+    );
+    charDiv
+      .append(charName)
+      .append(charImage)
+      .append(charHealth);
+    $(renderArea).append(charDiv);
+  };
+
+  var renderCharacters = function(charObj, areaRender) {};
+  if (areaRender === "#character-selection") {
+    $(areaRender).empty();
+    for (var key in charObj) {
+      if (charObj.hasOwnProperty(key)) {
+        renderOne(charObj[key], areaRender);
+      }
+    }
+  }
+
+  // Rendering all characters to the page when the game starts.
+  renderCharacters(characters, "#character-selection");
 });
